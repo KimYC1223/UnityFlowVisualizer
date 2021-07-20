01f72d616b430550e309e25cd4a0f8b6f0f81306
KimYC1223 <kau_esc@naver.com>
2021-07-20 19:15:50 +0900

    Init project

---
+++ Assets/UnityFlowVisualizer/Editor/PathGeneratorGUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityFlowVisualizer {
+    public class PathGeneratorGUI : EditorWindow {
+        public PathManager Env;
+        Texture LogoTex;
+        Texture EndTex;
+
+        [MenuItem("/Flow Visualizer/Total Path Manager")]
+        public static void ShowWindow() {
+            EditorWindow.GetWindow(typeof(PathGeneratorGUI));
+        }
+
+        public void OnEnable() {
+            LogoTex = (Texture2D)Resources.Load("UnityFlowVisualizer/Logo/FlowVisualizerLogo", typeof(Texture2D));
+            EndTex = (Texture2D)Resources.Load("UnityFlowVisualizer/Logo/FlowVisualizerLogo2", typeof(Texture2D));
+            Env = PathManager.Instance;
+        }
+
+        // Start is called before the first frame update
+        private void OnGUI() {
+            Env = PathManager.Instance;
+            try {
+                GameObject go = Env.gameObject;
+            } catch (System.Exception e) { e.ToString(); Env = null; }
+            GUILayout.Label(LogoTex);
+            GUILayout.Label("  Unity Flow Visualizer   |   유니티 유량 시각화 도구", EditorStyles.boldLabel);
+            GUILayout.Label("  Developed by KimYC1223");
+            GuiLine();
+            GUILayout.Space(10);
+            if (GUILayout.Button("Button", GUILayout.Height(30)))
+                EnvInitButton();
+
+            GUILayout.Label(EndTex);
+        }
+
+        void GuiLine(int i_height = 1) {
+            Rect rect = EditorGUILayout.GetControlRect(false, i_height);
+            rect.height = i_height;
+            EditorGUI.DrawRect(rect, new Color(0.5f, 0.5f, 0.5f, 1));
+        }
+
+        public void EnvInitButton() {
+            if (Env.gameObject != null) Debug.Log("Hello");
+            else Debug.Log("Null");
+        }
+    }
+}
+
+
---
+++ Assets/UnityFlowVisualizer/Scripts/PathManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFlowVisualizer {
+    [ExecuteInEditMode]
+    public class PathManager : MonoBehaviour
+    {
+        public static PathManager Instance;
+        public void OnEnable() {
+            this.gameObject.name = "PathManager";
+            Instance = this;
+        }
+    }
+}
+
